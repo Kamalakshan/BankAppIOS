0a48947d1fc177d83fc308a73a834dbbd27b959a
frankiejun <frankiejun@gmail.com>
2016-09-22 17:19:32 +0800

    Alamofire.request

---
+++ BankAppIOS/Md5String.swift
@@ -0,0 +1,29 @@
+//
+//  Md5String.swift
+//  
+//
+//  Created by frankie on 16/9/22.
+//
+//
+
+import Foundation
+
+extension String {
+    var md5 : String{
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = CC_LONG(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen);
+        
+        CC_MD5(str!, strLen, result);
+        
+        let hash = NSMutableString();
+        for i in 0 ..< digestLen {
+            hash.appendFormat("%02x", result[i]);
+        }
+        result.deinitialize();
+        
+        return String(format: hash as String)
+    }
+}
+
--- BankAppIOS/UserLogin.swift
+++ BankAppIOS/UserLogin.swift
@@ -1,5 +1,5 @@
 //
-//  userLogin.swift
+//  UserLogin.swift
 //  BankAppIOS
 //
 //  Created by frankie on 16/9/21.
@@ -8,13 +8,14 @@
 
 import Foundation
 import SwiftyJSON
+import Alamofire
 
 class UserLogin {
     var userName : String = ""
     var passWord : String = ""
     var devno : String = ""
     
-    init(foruserName userName : String, forpassWord passWord : String, fordevno devno:String) {
+    init (userName : String, passWord : String, devno:String) {
         self.userName = userName
         self.passWord = passWord
         self.devno = devno
@@ -22,10 +23,48 @@ class UserLogin {
     
     func login() {
         if(userName != "" && passWord != "" && devno != "") {
-            var jsonDate : JSON = [ "request":[
-            [ "loginname":userName, "password":passWord, "devno": devno]
-            ]]
-            print(jsonDate)
+            
+            let jsonDate : Parameters = [ "request": ["loginname":userName, "password":passWord.md5, "devno": devno]]
+            
+            //print(jsonDate)
+            //let urlString = "http://10.205.28.75:10080/appLogin"
+            let urlString = "http://api.jirengu.com/weather.php?callback=getWeather"
+          
+          
+            
+            Alamofire.request(urlString, method:.get, parameters: [:], encoding: JSONEncoding.default)
+                .validate()
+                .responseJSON { response in
+                    debugPrint(response)
+                    print(response.request)  // original URL request
+                    print(response.response) // URL response
+                    print(response.data!)     // server data
+                    print(response.result)   // result of response serialization
+
+
+                    if let ret = response.result.value{
+                        print("JSON: \(ret)") //具体如何解析json内容可看下方“响应处理”部分
+                    }
+            }
+//            Alamofire.request(urlString, method: .post, parameters: jsonDate, encoding: JSONEncoding.default)
+//                .response(queue: DispatchQueue.utility) { progress in
+//                    print("Progress: \(progress.fractionCompleted)")
+//                }
+//                .validate { request, response, data in
+//                    // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
+//                    return .success
+//                }
+//                .responseJSON { response in
+//                    debugPrint(response)
+//            }
+            //Alamofire.request(urlString, method: .post, parameters:jsonDate,encoding: JSONEncoding.default)
+            
+//           try  Alamofire.upload(jsonDate.rawData(), to: urlString).response { response in // method defaults to `.post`
+//                debugPrint(response)
+//            }
+            
+
+            
         }
     }
 }
